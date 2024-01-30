35c9812b3a342722d741ac4920a3b0bb5f00d31d
IronLegends7519 <drafin@edenschool.fr>
2024-01-30 14:58:02 +0100

    test replaceState

--- main.js
+++ main.js
@@ -1,2 +1,31 @@
 const app = document.querySelector("#app")
+let a = document.querySelector('#f')
+let b = document.querySelector('#g')
 
+a.addEventListener('click', ()=>{
+  history.replaceState("login", "", 'http://localhost:5173/login')
+
+})
+
+
+b.addEventListener('click', ()=>{
+  history.replaceState("home", "", 'http://localhost:5173/')
+})
+
+function createLogin (URL){
+  if(URL === 'http://localhost:5173/login'){
+    app.innerHTML=`<h1>connectez vous à votre compte</h1>`
+  }
+}
+
+function createMain(URL){
+  if(URL === 'http://localhost:5173/'){
+    app.innerHTML='<p>cool</p>'
+  }
+}
+
+
+
+createMain(document.location.href)
+createLogin(document.location.href)
+console.log(document.location.href)
